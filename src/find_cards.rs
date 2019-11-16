e6149bd1a44ba87223e190490bc6059c692a6e1d
Fenhl <fenhl@fenhl.net>
2019-11-16 12:52:19 +0000

    Add find-cards utility

---
+++ src/find_cards.rs
@@ -0,0 +1,17 @@
+//! A replacement of magic-search-engine's `find_cards` CLI frontend that uses the /list API.
+
+#![deny(rust_2018_idioms, unused, unused_import_braces, unused_qualifications, warnings)]
+
+use {
+    std::env,
+    itertools::Itertools as _,
+    lore_seeker::*
+};
+
+fn main() -> Result<(), Error> {
+    let (_, results) = resolve_query(&env::args().skip(1).join(" "))?;
+    for (card_name, _) in results {
+        println!("{}", card_name);
+    }
+    Ok(())
+}
